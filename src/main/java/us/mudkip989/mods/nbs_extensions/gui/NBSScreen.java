d1dd7addc259862f3f532093fd852cbeeec3a3f4
mudkip989 <muddcop@gmail.com>
2024-11-21 19:01:30 -0600

    GUI now displays and imports songs in norma format

--- src/main/java/us/mudkip989/mods/nbs_extensions/gui/NBSScreen.java
+++ src/main/java/us/mudkip989/mods/nbs_extensions/gui/NBSScreen.java
@@ -7,9 +7,15 @@ import net.minecraft.text.Text;
 import org.apache.logging.log4j.core.pattern.TextRenderer;
 import us.mudkip989.mods.nbs_extensions.*;
 import us.mudkip989.mods.nbs_extensions.gui.widget.*;
-import us.mudkip989.mods.nbs_extensions.util.MessageUtil;
+import us.mudkip989.mods.nbs_extensions.sys.*;
+import us.mudkip989.mods.nbs_extensions.util.*;
 
+import java.io.*;
+import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
+
+import static us.mudkip989.mods.nbs_extensions.nbs.NBSLoad.loadNbs;
 
 public class NBSScreen extends Screen {
 
@@ -25,15 +31,32 @@ public class NBSScreen extends Screen {
 
     @Override
     protected void init() {
-
-        thing2 = new SongListWidget(this.client, this.width, this.height - 80, 40, this.width - 160, 20);
-        for (int i = 0; i < 40; i++) {
-            thing2.add(new TextWidget(Text.literal("Test"), NBSExtensions.MC.textRenderer), ButtonWidget.builder(Text.literal((String.valueOf(random.nextInt(100)))), button -> {
-                MessageUtil.send("Funny" + String.valueOf(random.nextInt(100)));
-            }).dimensions(20, 20, 100, 20).build());
+        Stream<Path> files;
+        try {
+            files = Files.list(ExternalFile.NBS_FILES.getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        addDrawableChild(thing2);
+        thing2 = new SongListWidget(this.client, this.width, this.height - 80, 40, this.width - 160, 20);
+
+        files.forEach(p -> thing2.add(new TextWidget(Text.of(p.toFile().getName()), NBSExtensions.MC.textRenderer), ButtonWidget.builder(Text.literal("Import"), button -> {
+            if (NBSExtensions.MC.player != null && NBSExtensions.MC.player.isCreative()) {
+                loadNbs(p.toFile(), true);
+            }
+
+            }).dimensions(20, 20, 100, 20).build()));
+
+
+
+
+//        for (int i = 0; i < 40; i++) {
+//            thing2.add(new TextWidget(Text.literal("Test"), NBSExtensions.MC.textRenderer), ButtonWidget.builder(Text.literal((String.valueOf(random.nextInt(100)))), button -> {
+//                MessageUtil.send("Funny" + String.valueOf(random.nextInt(100)));
+//            }).dimensions(20, 20, 100, 20).build());
+//        }
+
+            addDrawableChild(thing2);
     }
 }
 
